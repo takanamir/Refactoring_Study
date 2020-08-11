9605457a02fc4df6175588083a251e93a01efc23
unknown <rtakanami0706@gmail.com>
2020-08-11 10:06:23 +0900

    (enum)シンボリック定数によるマジックナンバーの置き換え

---
+++ src/ReplaceMagicNumberWithSymbolicConstant/useenum/Main.java
@@ -0,0 +1,10 @@
+package ReplaceMagicNumberWithSymbolicConstant.useenum;
+
+public class Main {
+	public static void main(String[] args) {
+		Robot robot = new Robot("Andrew");
+		robot.order(Robot.Command.WALK);
+		robot.order(Robot.Command.STOP);
+		robot.order(Robot.Command.JUMP);
+	}
+}
