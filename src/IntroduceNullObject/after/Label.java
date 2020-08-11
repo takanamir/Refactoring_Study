cbda44633d4a5ecd10abab5ddd4fad67630226f5
unknown <rtakanami0706@gmail.com>
2020-08-11 15:47:28 +0900

    ヌルオブジェクトの導入

---
+++ src/IntroduceNullObject/after/Label.java
@@ -0,0 +1,21 @@
+package IntroduceNullObject.after;
+
+public class Label {
+	private final String _label;
+
+	public Label(String label) {
+		_label = label;
+	}
+
+	public void display() {
+		System.out.println("display: " + _label);
+	}
+
+	public String toString() {
+		return "\"" + _label + "\"";
+	}
+
+	public boolean isNull() {
+		return false;
+	}
+}
---
+++ src/IntroduceNullObject/before/MainTest.java
@@ -0,0 +1,58 @@
+package IntroduceNullObject.before;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.hyuki.refbook.StandardOutputTest;
+
+import junit.framework.JUnit4TestAdapter;
+
+public class MainTest extends StandardOutputTest {
+	@Test
+	public void testLabel() {
+		Label alice = new Label("Alice");
+		String expected = "\"Alice\"";
+		String actual = alice.toString();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testNullLabel() {
+		Label alice = new Label(null);
+		String expected = "\"null\"";
+		String actual = alice.toString();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testNullMail() {
+		Person alice = new Person(new Label("Alice"));
+		String expected = "[ Person: name=\"Alice\" mail=\"(none)\" ]";
+		String actual = alice.toString();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testToString() {
+		Person alice = new Person(new Label("Alice"), new Label("alice@example.com"));
+		String expected = "[ Person: name=\"Alice\" mail=\"alice@example.com\" ]";
+		String actual = alice.toString();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testDisplay() {
+		Person alice = new Person(new Label("Alice"), new Label("alice@example.com"));
+		alice.display();
+		String expected = getExpectedOutput(
+				"display: Alice",
+				"display: alice@example.com");
+		String actual = getActualOutput();
+		assertEquals(expected, actual);
+	}
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(MainTest.class);
+	}
+}
