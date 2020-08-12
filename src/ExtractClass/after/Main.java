426a9380dd9f60e22afebee1742c9e3fea9ec25e
unknown <rtakanami0706@gmail.com>
2020-08-12 12:30:09 +0900

    クラスの抽出

---
+++ src/ExtractClass/after/Main.java
@@ -0,0 +1,25 @@
+package ExtractClass.after;
+
+public class Main {
+	public static void main(String[] args) {
+		Book refactoring = new Book(
+				"Refactoring: improving the design of existing code",
+				"ISBN0201485672",
+				"$44.95",
+				"Martin Fowler",
+				"fowler@acm.org");
+
+		Book math = new Book(
+				"プログラマの数学",
+				"ISBN4797329734",
+				"\\2310",
+				"結城浩",
+				"hyuki@hyuki.com");
+
+		System.out.println("refactoring:");
+		System.out.println(refactoring.toXml());
+
+		System.out.println("math:");
+		System.out.println(math.toXml());
+	}
+}
---
+++ src/ExtractClass/before/MainTest.java
@@ -0,0 +1,34 @@
+package ExtractClass.before;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.JUnit4TestAdapter;
+
+public class MainTest {
+	@Test
+	public void testMain() {
+		Book book = new Book(
+				"The title of the book",
+				"ISBNxxxx",
+				"$12.34",
+				"A. U. Thor",
+				"author@example.com");
+		String actual = book.toXml();
+		String expected = "<book>"
+				+ "<title>The title of the book</title>"
+				+ "<isbn>ISBNxxxx</isbn>"
+				+ "<price>$12.34</price>"
+				+ "<author>"
+				+ "<name>A. U. Thor</name>"
+				+ "<mail>author@example.com</mail>"
+				+ "</author>"
+				+ "</book>";
+		assertEquals(expected, actual);
+	}
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(MainTest.class);
+	}
+}
