d59bdb06ff912875825c25c10ce3458db3bf67af
unknown <rtakanami0706@gmail.com>
2020-08-13 10:07:53 +0900

    Factory Method によるコンストラクタの置き換え

---
+++ src/ReplaceConstructorWithFactoryMethod/before/MainTest.java
@@ -0,0 +1,26 @@
+package ReplaceConstructorWithFactoryMethod.before;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.hyuki.refbook.StandardOutputTest;
+
+import junit.framework.JUnit4TestAdapter;
+
+public class MainTest extends StandardOutputTest {
+	@Test
+	public void testMain() {
+		Main.main(new String[0]);
+		String expected = getExpectedOutput(
+				"drawLine: [ LINE, (0, 0)-(100, 200) ]",
+				"drawRectangle: [ RECTANGLE, (10, 20)-(30, 40) ]",
+				"drawOval: [ OVAL, (100, 200)-(300, 400) ]");
+		String actual = getActualOutput();
+		assertEquals(expected, actual);
+	}
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(MainTest.class);
+	}
+}
